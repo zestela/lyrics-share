acdddd14d17d97e0f20bffe22a628293b0fdd357
LiangYin <liangyingugugu@163.com>
2024-06-30 11:30:33 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,120 @@
+<script setup>
+import shareView1 from "./views/shareView1.vue";
+</script>
+
+<template>
+  <el-header>
+    <img src="/logo.png" alt="" width="254" />
+  </el-header>
+  <el-main>
+    <div class="mt-4 search-box">
+      <el-input v-model="search_name" placeholder="请输入想分享的歌曲名称...">
+        <template #append><el-button @click="search_song">搜索</el-button></template>
+      </el-input>
+      <el-card id="search_suggestions">
+        <el-button v-for="(content, index) in song_list" :key="index" @click="setInfo(index)">{{ content.name + " " +
+          content.artist }}</el-button>
+      </el-card>
+    </div>
+    <div>
+      <img :src="song_coverUrl" alt="cover" width="87" id="coverImg" crossorigin="anonymous"/>
+      <div>
+        <el-input v-model="song_name" placeholder="歌曲名称" />
+        <el-input v-model="song_artist" placeholder="歌曲创作者" />
+      </div>
+    </div>
+    <el-input v-model="song_lyrics" rows="15" type="textarea" placeholder="歌曲歌词" />
+    <el-select v-model="selectedView" placeholder="海报样式" size="large" style="width: 240px" @change="$forceUpdate()">
+      <el-option label="样式1" value="shareView1" />
+      <el-option label="样式2" value="shareView2" />
+    </el-select>
+  </el-main>
+  <el-button type="primary" class="generation-btn" @click="generation_share">生成</el-button>
+  <component :is="selectedView" v-show="showView" :name="song_name" :artist="song_artist" :lyrics="song_lyrics"
+    :coverUrl="song_coverUrl" />
+</template>
+
+<script>
+import { extractColors } from 'extract-colors';
+import html2canvas from 'html2canvas'
+
+async function getJSON(url) {
+  try {
+    let response = await fetch(url);
+    return await response.json();
+  } catch (error) {
+    console.log("Request Failed", error);
+  }
+}
+
+export default {
+  data() {
+    return {
+      search_name: "",
+      song_name: "",
+      song_artist: "",
+      song_lyrics: "",
+      song_coverUrl: "./default-cover.png",
+      song_list: [],
+      selectedView: "shareView1",
+      showView: false,
+      api_url: "https://music.cyrilstudio.top"
+    };
+  },
+  components: { shareView1 },
+  watch: {
+    song_coverUrl() {
+      let imgElement = new Image();
+      imgElement.crossOrigin = 'Anonymous';
+      imgElement.src = this.song_coverUrl;
+      imgElement.onload = async function () {
+        let imgGetter = document.createElement("canvas");
+        imgGetter.getContext("2d").drawImage(this, 0, 0, 1024, 1024);
+        let colorLst = await extractColors(imgGetter.toDataURL("image/jpg"));
+        document.getElementsByClassName("share-body")[0].style.setProperty("background-color", colorLst[0].hex)
+      }
+    }
+  },
+  methods: {
+    async search_song() {
+      this.song_list = this.song_list.slice(0, 0);
+      let response = await getJSON(
+        this.api_url + "/search?keywords=" + this.search_name
+      );
+      if (response.result.songCount < 10)
+        response = response.result.songs.slice(0, response.result.songCount);
+      else response = response.result.songs.slice(0, 10);
+      for (let i = 0; i < response.length; i++)
+        this.song_list.push({
+          name: response[i].name,
+          artist: response[i].artists[0].name,
+          id: response[i].id,
+        });
+      document.getElementById("search_suggestions").style.setProperty("display", "block");
+    },
+    async setInfo(index) {
+      this.song_name = this.song_list[index].name;
+      let response = await getJSON(
+        this.api_url + "/lyric?id=" + this.song_list[index].id
+      );
+      this.song_lyrics = response.lrc.lyric.replace(/\[(.*?)\]/g, "");
+      response = await getJSON(
+        this.api_url + "/song/detail?ids=" + this.song_list[index].id
+      );
+      this.song_coverUrl = response.songs[0].al.picUrl;
+      this.song_artist = this.song_list[index].artist;
+      document.getElementById("search_suggestions").style.setProperty("display", "none");
+    },
+    async generation_share() {
+      this.showView = true;
+      html2canvas(document.getElementsByClassName("share-body")[0], { useCORS: true, scale: 2 }).then((canvas) => {
+      let img = new Image();
+      img.src = canvas.toDataURL('image/jpeg', 1.0);
+      let picWindow = window.open('', '_blank');
+      picWindow.document.write(img.outerHTML);
+      });
+    }
+  },
+  mounted() { },
+};
+</script>
---
+++ src/views/shareView2.vue
@@ -0,0 +1,98 @@
+<script setup>
+defineProps({
+  name: String,
+  artist: String,
+  coverUrl: String,
+  lyrics: String
+});
+</script>
+
+<template>
+  <div class="share-body">
+    <div class="main-card">
+      <div class="lyrics-card">
+        <div v-html="lyrics"></div>
+      </div>
+      <div class="info-card">
+        <img :src="coverUrl" height="50">
+        <div>
+          <div v-text="name"></div>
+          <div v-text="artist"></div>
+        </div>
+      </div>
+    </div>
+    <div class="logo">
+      <img src="/lyrics-share-logo.png" alt="LOGO" width="200">
+    </div>
+  </div>
+</template>
+
+<style>
+.share-body {
+  margin: 0;
+  width: 100%;
+  height: 100vh;
+  backdrop-filter: blur(30px);
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+
+  color: white;
+  font-family: "HarmonyOS Sans";
+}
+
+.main-card {
+  z-index: 100;
+  width: 50%;
+  height: max-content;
+  backdrop-filter: blur(30px);
+}
+
+@media screen and (min-width: 1024px) {
+  .main-card {
+    width: 25%;
+  }
+}
+
+.info-card {
+  background-color: rgba(88, 88, 88, 0.5);
+  margin: 0;
+  padding: 20px;
+  border-bottom-left-radius: 5px;
+  border-bottom-right-radius: 5px;
+  display: flex;
+  justify-content: left;
+  align-items: center;
+}
+
+.info-card img {
+  border-radius: 5px;
+}
+
+.info-card div {
+  margin-left: 10px;
+}
+
+.lyrics-card {
+  background-color: rgba(88, 88, 88, 0.4);
+  margin: 0;
+  padding: 20px;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+  white-space: break-spaces;
+}
+
+.lyrics-card * {
+  margin-bottom: 0;
+}
+
+.logo {
+  padding: 10px;
+  padding-bottom: 0;
+  border-radius: 5px;
+  position: fixed;
+  left: 5px;
+  bottom: 5px;
+}
+</style>
